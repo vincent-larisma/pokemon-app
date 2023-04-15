8950d51e48f74df333dbbd8c13df48054ec7a87b
Vince <103049779+vincent-larisma@users.noreply.github.com>
2023-04-15 10:50:24 +0800

    integrate scss to the styles

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Details() {
+  const [name, setName] = useState(null)
+
+  
   return (
     <>
       <div className='details-wrapper'>
