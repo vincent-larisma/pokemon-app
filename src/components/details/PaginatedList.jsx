7d9d9acf26721a8f8f302c3cd7871f4b8145db79
Vince <103049779+vincent-larisma@users.noreply.github.com>
2023-04-15 13:47:42 +0800

    add paginated list styles

--- src/components/details/PaginatedList.jsx
+++ src/components/details/PaginatedList.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 
 export default function PaginatedList() {
-
-    
   return (
     <>
       <div className='title'>Please Choose Your Starter Team!</div>
-      <div className='list'>
-
+      <div className='selectable'>
+        <div className='specification'>
+          <input type='checkbox' />
+          <img
+            src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png'
+            alt='pokeball'
+            height={96}
+          />
+          <div>Charizard</div>
+          <div>Type</div>
+        </div>
       </div>
     </>
   )
